4256844d298b155bd53f32cc948b850d69c9cd2e
VictorBellacosa <victorbellacosa1997@gmail.com>
2020-05-11 20:37:59 -0300

    Primeria classe: Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,14 @@
 package aplicacao;
 
+import bordajogo.Posicao;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Olá mundo !");
+		Posicao posicao = new Posicao(3, 5);
+		
+		System.out.println(posicao);
 
 	}
 
