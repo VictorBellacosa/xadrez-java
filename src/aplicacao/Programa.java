fb7e9608419f59466e1f5111dba3450d9aec3181
VictorBellacosa <victorbellacosa1997@gmail.com>
2020-05-11 20:49:34 -0300

    Implementada as classes Tabuleiro e Peca

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,14 +1,13 @@
 package aplicacao;
 
 import bordajogo.Posicao;
+import bordajogo.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		
-		Posicao posicao = new Posicao(3, 5);
-		
-		System.out.println(posicao);
+
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
 	}
 
