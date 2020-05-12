8080fc8b94bcdb8d6aab86ca30c7691e6c06f3a9
VictorBellacosa <victorbellacosa1997@gmail.com>
2020-05-11 21:17:42 -0300

    Camada de xadrez e imprimir o tabuleiro

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,13 @@
 package aplicacao;
 
-import bordajogo.Posicao;
-import bordajogo.Tabuleiro;
+import xadrez.XadrezPartida;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+		XadrezPartida xadrezPartida = new XadrezPartida();
+		UI.printTabuleiro(xadrezPartida.getPecas());
 
 	}
 
